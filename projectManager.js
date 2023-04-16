af2a6f86252c40ff255b6795062e6a5d09deae79
samu.el <samuelgauna78@gmail.com>
2023-04-15 23:32:20 -0300

    aplicandocambios

--- projectManager.js
+++ projectManager.js
@@ -1,38 +1,75 @@
+
+const fs = require('fs')
+
 class ProductManager {
     constructor(){
         this.products = []
+        this.path = './products.json'
     }
-    addProduct(title, description, price, thumbnail, id, stock){
-        if(title && description && price && thumbnail && id && stock){
-            const newProduct = {
-                title: title,
-                description: description,
-                price: price,
-                thumbnail: thumbnail,
-                id: id,
-                stock: stock
+    static id = 0
+    addProduct = async(title, description, price, thumbnail, stock, code)=>{
+        try{
+            if(title && description && price && thumbnail && stock && code){
+                const newProduct = {
+                    title: title,
+                    description: description,
+                    price: price,
+                    thumbnail: thumbnail,
+                    stock: stock,
+                    code: code,
+                    id: ProductManager.id++,
+                }
+                this.products.push(newProduct)
+                await fs.promises.writeFile(this.path, JSON.stringify(newProduct, null, 2))
+            } else {
+                console.error("Al producto le faltan datos")
             }
-            return this.products.push(newProduct)
-        } else {
-            console.error("Al producto le faltan datos")
+        }catch(error){
+            console.log(`error en el add products ${error}`)
+        }
+        
+    }
+    getProducts = async()=>{
+        try{
+            if(fs.existsSync(this.path)){
+                const readFileGetProducts = await fs.promises.readFile(this.path, "utf-8")
+                const parseGetProducts = JSON.parse(readFileGetProducts)
+                return parseGetProducts
+            }else {
+                await fs.promises.writeFile(this.path, "[]")
+            }
+        }catch(error){
+            console.log(`error en el get products ${error}`);
         }
     }
-    getProducts(){
-        console.log(this.products)
-        return this.products
-    }
-    getProductsById(id){
-        if(this.products.find(item => item.id == id)){
-            console.log('El producto que buscas si esta en tu inventario')
-        } else {
-            console.log("Not Found");
+    getProductsById = async(id)=>{
+        try{
+            if(fs.existsSync(this.path)){
+                const searchId = await fs.promises.readFile(this.path, "utf-8")
+                const searchIdParse = JSON.parse(searchId)
+                const productFind = searchIdParse.forEach((item)=>{
+                    if(item.id == id){
+                        console.log('El producto que buscas si esta en tu inventario')
+                        return productFind
+                    }
+                }) 
+                
+            } else {
+                console.log("Not Found");
+            }
+        }catch(error){
+            console.log(`error en el getProductsById ${error}`);
         }
+    }
+    updateProduct(){
+
+    }
+    deleteProduct(){
 
     }
 }
 
 const product = new ProductManager()
-product.addProduct("Escoba", "Semi nueva", 77, "https://http2.mlstatic.com/D_NQ_NP_2X_780827-MLA52027354635_102022-F.webp", 1, 5)
 product.getProducts()
-product.getProductsById(1)
-product.getProductsById(2)
+product.addProduct("Escoba", "Semi nueva", 77, "https://http2.mlstatic.com/D_NQ_NP_2X_780827-MLA52027354635_102022-F.webp", 5, 1545484)
+product.getProducts()
